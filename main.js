361ff8e685dbbe7688a9f175614205dbc8f481c0
brianr9799 <brianr9799@gmail.com>
2023-03-02 01:21:09 -0500

    updated css and js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('Your JS is linked');
+/*----- constants ----- */
+/*----- app's state (variables) ----- */
+/*----- cached element references ----- */
+/*----- event listeners ----- */
+/*----- functions ----- */
