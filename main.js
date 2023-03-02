dc4c6f6340027ed7abca294f61dbf44d5aeffb07
brianr9799 <brianr9799@gmail.com>
2023-03-02 02:54:38 -0500

    modified js-render x and o to screen

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
-console.log('Your JS is linked');
+//console.log('Your JS is linked');
 /*----- constants ----- */
 /*----- app's state (variables) ----- */
+
+let board;
+
+let turn = 'X';
+
 /*----- cached element references ----- */
+
+const squares = 
+Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners ----- */
+
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions ----- */
+
+function init() {
+    board = [
+        '','','',
+        '','','',
+        '','',''
+    ];
+    render();
+};
+
+init();
+
+function render () {
+    board.forEach(function(mark, index){
+        
+        //this sets the text content of the square of the same position to the mark on the board
+
+        squares[index].textContent = mark;
+        //console.log(mark, index);
+    });
+};
+
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+        return square === event.target;
+    });
+
+    board[idx] = turn;
+
+    turn = turn === 'X' ? 'O' : 'X';
+
+
+    render();
+};
