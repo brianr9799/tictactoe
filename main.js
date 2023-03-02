bf9fac9d7caf88b46538451c8bdca16ba6c843c1
brianr9799 <brianr9799@gmail.com>
2023-03-02 18:12:19 -0500

    basic game finished

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 //console.log('Your JS is linked');
 /*----- constants ----- */
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
 /*----- app's state (variables) ----- */
 
 let board;
 
 let turn = 'X';
 
+let win;
+
 /*----- cached element references ----- */
 
 const squares = 
 Array.from(document.querySelectorAll('#board div'));
+
+const messages = document.querySelector('h2');
 /*----- event listeners ----- */
 
 document.getElementById('board').addEventListener('click', handleTurn);
+
+document.getElementById('reset-button').addEventListener('click',init);
 /*----- functions ----- */
 
 function init() {
@@ -27,13 +44,14 @@ function init() {
 init();
 
 function render () {
-    board.forEach(function(mark, index){
+    board.forEach(function(val, idx){
         
         //this sets the text content of the square of the same position to the mark on the board
 
-        squares[index].textContent = mark;
+        squares[idx].textContent = val;
         //console.log(mark, index);
     });
+    messages.textContent = win === 'T' ? `That's a tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
 
 function handleTurn(event) {
@@ -43,8 +61,23 @@ function handleTurn(event) {
 
     board[idx] = turn;
 
+    win = getWinner();
+
     turn = turn === 'X' ? 'O' : 'X';
 
 
     render();
+};
+
+function getWinner() {
+
+    let winner = null;
+
+    winningCombos.forEach(function(combo, index) {
+
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+            winner = board[combo[0]];
+        }
+    });
+    return winner ? winner : board.includes('') ? null : 'T';
 };
